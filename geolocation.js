40ed7aec93750adb458a657b93caeb659da40521
sinocow <153889011+sinocow@users.noreply.github.com>
2025-01-17 15:52:45 +0900

    Update geolocation.js

--- geolocation.js
+++ geolocation.js
@@ -1,7 +1,7 @@
 // ボタンを押した時の処理
 document.getElementById("btn").onclick = function () {
   // 位置情報を取得する
-  navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+  navigator.geolocation.getCurrentPosition(successCallback, errorCallback,test2);
 };
 
 // 取得に成功した場合の処理
@@ -20,4 +20,39 @@ function successCallback(position) {
 // 取得に失敗した場合の処理
 function errorCallback(error) {
   alert("位置情報が取得できませんでした");
+}
+
+function test2(position) {
+
+    //まず現在地の緯度経度を取得する
+    var lat = position.coords.latitude;
+    var lon = position.coords.longitude;
+
+    //国土地理院API用に有効桁数を合わせる。
+    var adjustiveLat = lat + "00";
+    var adjustiveLon = lon + "0";
+
+    //文字列に変換
+    var stringLat = String(adjustiveLat);
+    var stringLon = String(adjustiveLon);
+
+    //国土地理院APIに現在地の緯度経度を渡して、標高を取得する
+    const url = 'http://cyberjapandata2.gsi.go.jp/general/dem/scripts/getelevation.php?lon=' + stringLon + '&lat=' + stringLat + '&outtype=JSON';
+
+    console.log(url);
+
+    fetch(url).then(function(response) {
+      return response.text();
+    }).then(function(text) {
+      console.log(text);
+      
+      //取得したjsonをパース
+      var jsonAltitude = JSON.parse(text).elavation;
+      console.log("標高：" + jsonAltitude.elevation + "m");
+
+      //ポップアップ表示
+      alert("現在地の標高は" + jsonAltitude.elevation + "mです。" +  "(" + "緯度：" + stringLat + "、経度：" + stringLon + ")")
+
+    });
+
 }
